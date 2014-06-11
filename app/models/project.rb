86080f1a327713a4feed444395c72673805d277d
Daniel Ursinus <danel@ursinus.de>
2014-06-11 16:31:20 +0200

    nested routes mit tickets erweitert

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ActiveRecord::Base
 	validates :name, presence: true
 
-	has_many :tickets
-	
+	has_many :tickets, dependent: :delete_all # oder :nullify (Tickets werden nicht gelöscht, es wird lediglich die Project ID rausgenommen)
+	# MySQL query: UPDATE tickets SET project_id = NULL WHERE project_id = :project_id
+	# :destroy - der query dauert aber länger
+
 end
